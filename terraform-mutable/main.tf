e1c0cb03cbd1c2bd9b5c46c36b937647ec311895
vasu <bandaruvasu1@gmail.com>
2022-04-04 10:07:17 +0530

    commit

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -1,5 +1,8 @@
 module "sample" {
   source = "git::https://github.com/vasubandaru1/terraform-mutable.git//app-module"
   INSTANCE_TYPE = var.INSTANCE_TYPE
+  ENV   = var.ENV
+  OD_INSTANCE_COUNT = var.OD_INSTANCE_COUNT
+  SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
 
 }
