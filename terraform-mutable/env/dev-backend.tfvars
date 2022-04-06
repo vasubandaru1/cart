3ac7605c159955f36b0867cd98dd9127203a39b3
vasu <bandaruvasu1@gmail.com>
2022-04-06 09:26:08 +0530

    commit

--- terraform-mutable/env/dev-backend.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "vasudevops"
-key    = "terraform-mutable/cart/dev/terraform.tfstate"
-region = "us-east-1"
--- terraform-mutable/env/dev.tf
+++
@@ -1,4 +0,0 @@
-ENV                   = "dev"
-INSTANCE_TYPE         = "t2.micro"
-SPOT_INSTANCE_COUNT   = 1
-OD_INSTANCE_COUNT     = 1
